69b6f7345a58d3c4d933dd728541c3a026d0d819
Alex Gheorghita <alex.gheorghyta@gmail.com>
2023-12-21 19:21:12 +0200

    Add logic for starting and stopping the game.

--- GameManager.cs
+++ GameManager.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartGame()
     {
-        
+        // Starting a game means loading a scene.
+        // Because we have only one scene we want to load it.
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StopGame()
     {
-        
+        // The following is a preprocessor directive.
+        // It tells the compiler to compile only if the statement is met (meaning, only if we are in the Unity Editor).
+#if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
     }
 }
