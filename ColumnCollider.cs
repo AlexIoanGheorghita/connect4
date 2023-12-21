09fd10ede7c73dc280cffb5e25a4723032e6f1bb
Alex Gheorghita <alex.gheorghyta@gmail.com>
2023-12-21 19:32:18 +0200

    Add fields to a column object that describe the position of a circle object.

---
+++ ColumnCollider.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnCollider : MonoBehaviour
+{
+    public int column;
+    public Vector3 initialPosition;
+    public Vector3 targetPosition;
+}
